4431e072fbf50e3ee8b9809cf7635ec8747f651d
atlanticglobalexpress <expressatlanticglobal@gmail.com>
2021-02-12 09:08:20 +0100

    adding static

--- Omoh_Shiping/settings.py
+++ Omoh_Shiping/settings.py
@@ -137,7 +137,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
 
+    os.path.join(BASE_DIR, "static"),
+]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
